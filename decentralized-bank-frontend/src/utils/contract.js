d5f25581741ac630d4715aa12f8806f15a366587
GMORETT <101185279+GMORETT@users.noreply.github.com>
2025-01-13 23:48:22 -0300

    Minor changes

--- decentralized-bank-frontend/src/utils/contract.js
+++ decentralized-bank-frontend/src/utils/contract.js
@@ -1,14 +1,14 @@
 import { ethers } from "ethers";
 
-const contractAddress = "0xd5ADEFdCecd2C09eFc5d22E5BF3845CC007710F0"; // Substitua pelo endereço do contrato
+const contractAddress = "<YOUR_CONTRACT_CODE>"; // Substitua pelo endereço do contrato
 const contractABI = require("./DecentralizedBankABI.json");
 
 export const getContract = async () => {
     if (window.ethereum) {
-        const provider = new ethers.BrowserProvider(window.ethereum); // Configuração do provedor
-        const accounts = await provider.send("eth_requestAccounts", []); // Solicita acesso à conta
-        const signer = await provider.getSigner(accounts[0]); // Obtém o signer da conta conectada
-        return new ethers.Contract(contractAddress, contractABI, signer); // Retorna o contrato com o signer
+        const provider = new ethers.BrowserProvider(window.ethereum); 
+        const accounts = await provider.send("eth_requestAccounts", []);
+        const signer = await provider.getSigner(accounts[0]); 
+        return new ethers.Contract(contractAddress, contractABI, signer); 
     } else {
         alert("MetaMask não encontrada!");
         throw new Error("MetaMask não encontrada!");
